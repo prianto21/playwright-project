f5d4cf828e940f359efbb7f1a79912239f4fb362
Ranto Prianto Purba <ranto.prianto@sharingvision.co.id>
2026-08-26 16:20:23 +0700

    add new test for practice

---
+++ tests/example2.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+
+test('Open Google', async ({ page }) => {
+    await page.goto('https://www.google.com');
+
+    await expect(page).toHaveTitle(/Google/);
+    // await expect(page.getByRole('textbox', { name: 'Search' })).toBeVisible();
+    const searchBox = page.getByRole('textbox', { name: 'q' });
+    await searchBox.fill('Playwright');
+    await expect(searchBox).toHaveValue('Playwright');
+});
---
+++ tests/login.spec.ts
@@ -0,0 +1,47 @@
+import { test, expect } from '@playwright/test';
+
+// <input type="text" aria-label="Username">
+// <input type="password" aria-label="Password">
+// <button>Login</button>
+
+test('Login', async ({ page }) => {
+
+    // 1. buka halaman login
+
+    // 2. ambil username
+    const usernameInput = page.getByRole('textbox', { name: 'Username' });
+    // 3. isi username dengan "alex"
+    await usernameInput.fill('alex');
+    // 4. ambil password
+    const passwordInput = page.getByLabel('password')
+    // 5. isi password dengan "123456"
+    await passwordInput.fill('123456'); 
+    // 6. ambil button Login
+    const loginButton = page.getByRole('button', { name: 'Login' });
+    // 7. klik Login
+    await loginButton.click();
+});
+
+
+// Transaction List
+
+// Transaction ID    Status       Action
+// ---------------------------------------------
+// TRX-1001          SUCCESS      [View]
+// TRX-1002          FAILED       [View]
+// TRX-1003          SUCCESS      [View]
+
+test('view failed details', async ({ page }) => {
+
+   const transactionRow = page
+    .getByRole('row')
+    .filter({ hasText: 'TRX-1002' });
+
+    await transactionRow
+    .getByRole('button', { name: 'View' })
+    .click();
+
+    await expect(
+        page.getByText('TRX-1002')
+    ).toBeVisible();
+});
